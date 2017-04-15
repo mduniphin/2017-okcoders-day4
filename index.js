d22bc621b6d2507d9fe6946ace0f94365f1bad4a
mduniphin <maxx.duniphin@gmail.com>
2017-04-15 12:31:36 -0500

    more cleanup

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var server = restify.createServer();
 var port = 8088;
 
 var hello = require('./routes/hello.js');
+var emails = require('./routes/emails.js')
 var client = require('./client');
-var Emails = require('./models/emails.js')
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/test')
@@ -18,26 +18,16 @@ db.once('open', function(){
 	console.log("Mongoose connection established");
 });
 
-function getEmails(req, res, next){
-	Emails.find({}).limit(10).exec(function(err, data){
-		if(err){res.send('Error');}
-		else {
-			res.json(data);
-		}
-	});
-	return next();
-}
-
 // Client
 server.get('/', client.get);
 
 // Server responses
-server.get('/emails', getEmails);
+server.get('/emails', emails.get);
 server.get('/hello/:name', hello.send);
 server.put('/hello/:name', hello.send);
 server.post('/hello/:name', hello.post);
-server.del('hello/:name', hello.del);
+server.del('/hello/:name', hello.del);
 
-server.listen(port, function() {
+server.listen(port, function(){
 	console.log('%s listening at %s', server.name, port);
 });
